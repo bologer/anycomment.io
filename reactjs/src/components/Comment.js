c0a12d1aff6054ee034f2de45b3ca6c80cbe0487
Alex <sasha.tesh@gmail.com>
2018-08-12 22:31:51 +0200

    detect links, youtube and make them as attachments, #69

--- reactjs/src/components/Comment.js
+++ reactjs/src/components/Comment.js
@@ -4,6 +4,7 @@ import CommentAvatar from './CommentAvatar';
 import CommentHeader from './CommentHeader';
 import CommentFooter from './CommentFooter';
 import {toast} from 'react-toastify';
+import reactStringReplace from 'react-string-replace';
 
 /**
  * Comment is rendering single comment entry.
@@ -82,6 +83,92 @@ class Comment extends AnyCommentComponent {
         this.props.handleDelete(comment);
     }
 
+    processContent(comment) {
+        let content = comment.content;
+
+        const linksRe = new RegExp(
+            // protocol identifier
+            "((?:(?:https?|ftp)://)" +
+            // user:pass authentication
+            "(?:\\S+(?::\\S*)?@)?" +
+            "(?:" +
+            // IP address exclusion
+            // private & local networks
+            "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+            "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+            "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+            // IP address dotted notation octets
+            // excludes loopback network 0.0.0.0
+            // excludes reserved space >= 224.0.0.0
+            // excludes network & broacast addresses
+            // (first & last IP address of each class)
+            "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+            "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+            "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+            "|" +
+            // host name
+            "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+            // domain name
+            "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+            // TLD identifier
+            "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+            // sorry, ignore TLD ending with dot
+            // "\\.?" +
+            ")" +
+            // port number
+            "(?::\\d{2,5})?" +
+            // resource path, excluding a trailing punctuation mark
+            "(?:[/?#](?:\\S*[^\\s!\"'()*,-.:;<>?\\[\\]_`{|}~]|))?)"
+            , "gi"
+        );
+
+        content = reactStringReplace(content, linksRe, (match, i) => (
+            <a key={match + i} href={match} target="_blank" rel="noreferrer noopener">{match}</a>
+        ));
+
+        return content;
+    };
+
+    processAttachments(comment) {
+        let attachments = [];
+
+        const youtubeRe = /(^(https?:\/\/)?((www\.)?youtube\.com|youtu\.?be)\/.+$)/gi;
+        const imageRe = /((http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|svg))/gi;
+
+
+
+
+        const youtubeMatches = comment.content.match(youtubeRe);
+        const imageMatches = comment.content.match(imageRe);
+
+
+        if (youtubeMatches) {
+            for (let i = 0; i < youtubeMatches.length; i++) {
+                attachments.push(<li><a className="comment-attachment comment-attachment__link" href={youtubeMatches[i]}
+                                        target="_blank"
+                                        rel="noreferrer noopener"></a></li>);
+            }
+        }
+
+        if (imageMatches) {
+            for (let k = 0; k < imageMatches.length; k++) {
+                attachments.push(<li><a className="comment-attachment comment-attachment__image" href={imageMatches[k]}
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                        style={{backgroundImage: 'url(' + imageMatches[k] + ')'}}></a></li>);
+            }
+        }
+
+
+
+        if (!attachments) {
+            return null;
+        }
+
+
+        return <ul className="comment-attachments clearfix">{attachments}</ul>;
+    }
+
     render() {
         const comment = this.props.comment;
 
@@ -108,8 +195,10 @@ class Comment extends AnyCommentComponent {
                     <CommentHeader comment={comment}/>
 
                     <div className="comment-single-body__text">
-                        <p>{comment.content}</p>
+                        <p>{this.processContent(comment)}</p>
                     </div>
+
+                    {this.processAttachments(comment)}
 
                     <CommentFooter
                         onEdit={this.onEdit}
@@ -125,6 +214,8 @@ class Comment extends AnyCommentComponent {
             </li>
         )
     }
+
+
 }
 
 export default Comment;
