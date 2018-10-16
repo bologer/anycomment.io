a1f1bf875b197b537a59e32ab311711891a99130
mihdan <mikhail@kobzarev.com>
2018-10-16 18:28:45 +0300

    Поправил миграцию 0.0.61

--- includes/migrations/AnyCommentMigration_0_0_61.php
+++ includes/migrations/AnyCommentMigration_0_0_61.php
@@ -1,7 +1,7 @@
 <?php
 
 class AnyCommentMigration_0_0_61 extends AnyCommentMigration {
-	public $table = 'rating';
+	public $table   = 'rating';
 	public $version = '0.0.61';
 
 	/**
@@ -20,26 +20,28 @@ class AnyCommentMigration_0_0_61 extends AnyCommentMigration {
 	public function up() {
 		global $wpdb;
 
+		$charset_collate = $wpdb->get_charset_collate();
+
 		/**
 		 * Create email queue table
 		 */
 		$sql = "CREATE TABLE `{$this->getTable()}` (
-  `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `post_ID` bigint(20) UNSIGNED NOT NULL,
-  `user_ID` bigint(20) UNSIGNED DEFAULT NULL,
-  `rating` smallint(1) SIGNED DEFAULT 5,
-  `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_agent` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` bigint(20) NOT NULL,
-  INDEX post_ID (`post_ID`),
-  INDEX user_ID (`user_ID`),
-  FOREIGN KEY (post_ID)
-        REFERENCES {$wpdb->posts}(ID)
-        ON DELETE CASCADE,
-  FOREIGN KEY (user_ID)
-        REFERENCES {$wpdb->users}(ID)
-        ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+				  `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				  `post_ID` bigint(20) UNSIGNED NOT NULL,
+				  `user_ID` bigint(20) UNSIGNED DEFAULT NULL,
+				  `rating` smallint(1) SIGNED DEFAULT 5,
+				  `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+				  `user_agent` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+				  `created_at` bigint(20) NOT NULL,
+				  INDEX post_ID (`post_ID`),
+				  INDEX user_ID (`user_ID`),
+				  FOREIGN KEY (post_ID)
+				        REFERENCES {$wpdb->posts}(ID)
+				        ON DELETE CASCADE,
+				  FOREIGN KEY (user_ID)
+				        REFERENCES {$wpdb->users}(ID)
+				        ON DELETE CASCADE
+				) $charset_collate;";
 
 		return $wpdb->query( $sql ) !== false;
 	}
@@ -53,3 +55,5 @@ class AnyCommentMigration_0_0_61 extends AnyCommentMigration {
 		$wpdb->query( $sql );
 	}
 }
+
+// eof;
