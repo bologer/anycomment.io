ae45625ac87455ba6b9da876e2de23108e61607a
Alex <sasha.tesh@gmail.com>
2018-10-20 13:21:32 +0200

    towards new desing

--- reactjs/src/components/CommentFooter.js
+++ reactjs/src/components/CommentFooter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AnyCommentComponent from "./AnyCommentComponent";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faReply, faHeart, faEdit} from '@fortawesome/free-solid-svg-icons'
 
 /**
  * CommentFooter renders single comment actions such as
@@ -12,24 +14,28 @@ class CommentFooter extends AnyCommentComponent {
         const comment = this.props.comment;
         const isGuest = this.isGuest();
 
+        const replyIcon = <FontAwesomeIcon icon={faReply}/>;
+        const likeIcon = <FontAwesomeIcon icon={faHeart} style={this.props.hasLike ? {color: '#EC4568'} : ''}/>;
+        const editIcon = <FontAwesomeIcon icon={faEdit}/>;
+
         return (
             <footer className="anycomment comment-single-body__actions">
                 <ul className="anycomment">
-                    <li className="anycomment"><a className="anycomment" href=""
-                                                  onClick={(e) => this.props.onReply(e)}>{settings.i18.reply}</a>
+                    <li className="anycomment"><a className="anycomment" href="javascript:void(0)"
+                                                  onClick={(e) => this.props.onReply(e)}>{replyIcon}{settings.i18.reply}</a>
                     </li>
                     <li className="anycomment">
-                            <span
-                                className={"anycomment comment-single-body__actions-like " + (this.props.hasLike ? 'active' : '') + ""}
-                                onClick={(e) => this.props.onLike(e)}>{this.props.likesCount}</span>
+                        <a href="javascript:void(0)"
+                           className="anycomment comment-single-body__actions-like"
+                           onClick={(e) => this.props.onLike(e)}>{likeIcon}{this.props.likesCount}</a>
                     </li>
 
                     {!isGuest && comment.permissions.can_edit_comment ?
-                        <li className="anycomment"><a className="anycomment" href=""
-                                                      onClick={(e) => this.props.onEdit(e)}>{settings.i18.edit}</a>
+                        <li className="anycomment"><a className="anycomment" href="javascript:void(0)"
+                                                      onClick={(e) => this.props.onEdit(e)}>{editIcon}{settings.i18.edit}</a>
                         </li> : ''}
                     {!isGuest && comment.permissions.can_edit_comment ?
-                        <li className="anycomment"><a className="anycomment" href=""
+                        <li className="anycomment"><a className="anycomment" href="javascript:void(0)"
                                                       onClick={(e) => this.props.onDelete(e, comment)}>{settings.i18.delete}</a>
                         </li> : ''}
                 </ul>
