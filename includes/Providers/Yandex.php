4c5578876c8570ff194227380ef3bf8fc84cd50d
Alex <sasha.tesh@gmail.com>
2018-12-01 16:18:52 +0100

    added ZIP build shell script helper + small fixed for Yandex provider, #282

--- includes/Providers/Yandex.php
+++ includes/Providers/Yandex.php
@@ -10,12 +10,30 @@ use Hybridauth\Exception\UnexpectedApiResponseException;
 use Hybridauth\User\Profile;
 
 /**
- * Class Yandex is a provider for Hybriauth library.
+ * Yandex provider adapter.
  *
- * @author Alexander Teshabaev <sasha.tesh@gmail.com>
- * @package AnyComment\Providers
+ * Example:
+ *
+ *   $config = [
+ *       'callback'  => Hybridauth\HttpClient\Util::getCurrentUrl(),
+ *       'keys'      => ['id' => '', 'secret' => ''],
+ *   ];
+ *
+ *   $adapter = new Hybridauth\Provider\Yandex($config);
+ *
+ *   try {
+ *       if (!$adapter->isConnected()) {
+ *           $adapter->authenticate();
+ *       }
+ *
+ *       $userProfile = $adapter->getUserProfile();
+ *   }
+ *   catch(\Exception $e) {
+ *       print $e->getMessage() ;
+ *   }
  */
-class Yandex extends OAuth2 {
+class Yandex extends OAuth2
+{
 	/**
 	 * {@inheritdoc}
 	 */
@@ -32,66 +50,44 @@ class Yandex extends OAuth2 {
 	protected $accessTokenUrl = 'https://oauth.yandex.ru/token';
 
 	/**
-	 * {@inheritdoc}
-	 */
-	public function hasAccessTokenExpired() {
-		// As we using offline scope, $expired will be false.
-		$expired = $this->getStoredData( 'expires_in' )
-			? $this->getStoredData( 'expires_at' ) <= time()
-			: false;
-
-		return $expired;
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function validateAccessTokenExchange( $response ) {
-		$data = parent::validateAccessTokenExchange( $response );
-
-		// Need to store user_id as token for later use.
-		$this->storeData( 'user_id', $data->get( 'user_id' ) );
-		$this->storeData( 'email', $data->get( 'email' ) );
-	}
-
-	/**
 	 * load the user profile from the IDp api client
 	 *
 	 * @throws Exception
 	 */
-	public function getUserProfile() {
+	public function getUserProfile()
+	{
 
-		$this->scope = implode( ',', [] );
+		$this->scope = implode(',', []);
 
-		$response = $this->apiRequest( $this->apiBaseUrl . "?format=json", 'GET' );
+		$response = $this->apiRequest($this->apiBaseUrl . "?format=json");
 
-		if ( ! isset( $response->id ) ) {
-			throw new UnexpectedApiResponseException( "User profile request failed! {$this->providerId} returned an invalid response.", 6 );
+		if (!isset($response->id)) {
+			throw new UnexpectedApiResponseException("User profile request failed! {$this->providerId} returned an invalid response.", 6);
 		}
 
-		$data = new Collection( $response );
+		$data = new Collection($response);
 
-		if ( ! $data->exists( 'id' ) ) {
-			throw new UnexpectedApiResponseException( 'Provider API returned an unexpected response.' );
+		if (!$data->exists('id')) {
+			throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
 		}
 
 		$userProfile = new Profile();
 
-		$userProfile->identifier    = $data->get( 'id' );
-		$userProfile->firstName     = $data->get( 'real_name' );
-		$userProfile->lastName      = $data->get( 'family_name' );
-		$userProfile->displayName   = $data->get( 'display_name' );
-		$userProfile->photoURL      = 'http://upics.yandex.net/' . $userProfile->identifier . '/normal';
-		$userProfile->profileURL    = "";
-		$userProfile->gender        = $data->get( 'sex' );
-		$userProfile->email         = $data->get( 'default_email' );
-		$userProfile->emailVerified = $data->get( 'default_email' );
+		$userProfile->identifier = $data->get('id');
+		$userProfile->firstName = $data->get('real_name');
+		$userProfile->lastName = $data->get('family_name');
+		$userProfile->displayName = $data->get('display_name');
+		$userProfile->photoURL = 'http://upics.yandex.net/' . $userProfile->identifier . '/normal';
+		$userProfile->profileURL = "";
+		$userProfile->gender = $data->get('sex');
+		$userProfile->email = $data->get('default_email');
+		$userProfile->emailVerified = $data->get('default_email');
 
-		if ( $data->get( 'birthday' ) ) {
-			list( $birthday_year, $birthday_month, $birthday_day ) = explode( '-', $response->birthday );
-			$userProfile->birthDay   = (int) $birthday_day;
-			$userProfile->birthMonth = (int) $birthday_month;
-			$userProfile->birthYear  = (int) $birthday_year;
+		if ($data->get('birthday')) {
+			list($birthday_year, $birthday_month, $birthday_day) = explode('-', $response->birthday);
+			$userProfile->birthDay = (int)$birthday_day;
+			$userProfile->birthMonth = (int)$birthday_month;
+			$userProfile->birthYear = (int)$birthday_year;
 		}
 
 		return $userProfile;
---
+++ scripts/build.sh
@@ -0,0 +1,51 @@
+# Script is meant to package plugin into ZIP archive so it can be easily used for testing purposes
+# This script SHOULD NOT be used for release purposes
+
+# Temp DIR where to store code
+TEMP_PATH="/tmp"
+
+# ZIP file name
+ZIP_FILE_NAME=anycomment
+
+read -p "Enter plugin root: " plugin_root
+echo
+read -p "Enter archive path: " save_path
+echo
+echo "Trying to get files in $plugin_root and then save archive in $save_path"
+echo
+cd ..
+
+rsync -arv --cvs-exclude \
+    --exclude='.gitignore' \
+    --exclude='assets/src' \
+    --exclude='scripts' \
+    --exclude='reactjs' \
+    --exclude='test' \
+    --exclude='CHANGELOG.md' \
+    --exclude='README.md' \
+    --exclude='node_modules' \
+    --exclude='gulpfile.js' \
+    --exclude='package.json' \
+    --exclude='package-lock.json' \
+    --exclude='.travis.yml' \
+    --exclude='phpunit.xml' \
+    --exclude='vendor/bin' \
+    $plugin_root \
+    $TEMP_PATH
+
+
+# Packaging
+echo "Ready to ZIP package in $TEMP_PATH and move to $save_path"
+cd $TEMP_PATH
+zip -r $ZIP_FILE_NAME anycomment/*
+echo "Moving "${TEMP_PATH}/${ZIP_FILE_NAME}.zip" to $save_path"
+mv "${TEMP_PATH}/${ZIP_FILE_NAME}.zip" $save_path
+
+# Cleaning
+#echo "ZIP should've been moved, now cleaning"
+#rm -rf "$TEMP_PATH.zip"
+
+# Done
+echo "============= DONE ============="
+echo "ZIP located in ${TEMP_PATH}/${save_path}.zip"
+echo "============= DONE ============="
