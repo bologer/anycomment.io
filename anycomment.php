40de3c077374899da8c5cce5df7bf84a78088450
Alex <sasha.tesh@gmail.com>
2019-03-10 16:18:49 +0100

    versions bump [ci skip]

--- anycomment.php
+++ anycomment.php
@@ -3,7 +3,7 @@
  * Plugin Name: AnyComment
  * Plugin URI: https://anycomment.io
  * Description: AnyComment is an advanced commenting system for WordPress.
- * Version: 0.0.85
+ * Version: 0.0.86
  * Author: Bologer
  * Author URI: http://bologer.ru
  * Requires at least: 4.4
